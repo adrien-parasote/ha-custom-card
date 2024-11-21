073547aec691fed0248bd1c671045f04f3c801c0
adrien-parasote <adrien.parasote@gmail.com>
2024-11-21 13:26:46 +0100

    Fix prod/dev env topi

--- src/const.js
+++ src/const.js
@@ -1,5 +1,3 @@
-//import { version } from "../package.json";
-
 export const PACKAGE_NAME = "sci-fi";
-export const VERSION = "1.0"; // TODO review for packaging
-export const DEV = false; // TODO review for packaging
+export const VERSION = "dev";
+export const DEV = true;
